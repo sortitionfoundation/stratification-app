72eb7cfc17d43db00f02b99ebffa9873bb0e159d
Paul Gölz <pgoelz@cs.cmu.edu>
2021-03-30 15:22:55 -0400

    Grant additional license permission to link with proprietary library Gurobi.

--- stratification.py
+++ stratification.py
@@ -1,9 +1,23 @@
 """
-    Python (3) script to do a stratified, random selection from respondents to random mail out
+Python (3) script to do a stratified, random selection from respondents to random mail out
 
-    written by Brett Hennig bsh [AT] sortitionfoundation.org
-    contributions by Paul Gölz pgoelz (AT) cs.cmu.edu
+Copyright (C) 2019-2021 Brett Hennig bsh [AT] sortitionfoundation.org & Paul Gölz pgoelz (AT) cs.cmu.edu
 
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; if not, see
+<https://www.gnu.org/licenses>.
+
+Additional permission under GNU GPL version 3 section 7
+
+If you modify this Program, or any covered work, by linking or combining it with Gurobi (or a modified version of that
+library), containing parts covered by the terms of GUROBI OPTIMIZATION, LLC END-USER LICENSE AGREEMENT, the licensors of
+this Program grant you additional permission to convey the resulting work.
 """
 import codecs
 import copy
